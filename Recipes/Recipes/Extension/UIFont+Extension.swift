c521adee9b5f76ddead1c95da4950b6adc944400
Halimat <m.h.axm@mail.ru>
2024-03-06 21:06:44 +0300

    change extension

--- Recipes/Recipes/Extension/UIFont+Extension.swift
+++ Recipes/Recipes/Extension/UIFont+Extension.swift
@@ -4,27 +4,20 @@
 import UIKit
 
 extension UIFont {
-    
     private static var fontCache = [String: UIFont]()
-    
+
     static func cachedFont(name: String, size: CGFloat) -> UIFont {
-        
         let fontKey = "\(name)-\(size)"
-        
+
         if let cachedFont = fontCache[fontKey] {
-            
             return cachedFont
-            
+
         } else {
-            
             let font = UIFont(name: name, size: size)!
-            
+
             fontCache[fontKey] = font
-            
+
             return font
-            
         }
-        
     }
-    
 }
