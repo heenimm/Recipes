bc0c8c23db90c8a8883bd6d839d60a47d07be3b1
Halimat <m.h.axm@mail.ru>
2024-03-06 11:55:21 +0300

    fix bug

--- Recipes/Recipes/Extension/UIColor+Extension.swift
+++ Recipes/Recipes/Extension/UIColor+Extension.swift
@@ -3,4 +3,25 @@
 
 import UIKit
 
-extension UIColor {}
+extension UIColor {
+    static var colorStore: [String: UIColor] = [:]
+    class func rgba(
+        _ red: CGFloat,
+        _ green: CGFloat,
+        _ blue: CGFloat,
+        _ alpha: CGFloat
+    ) -> UIColor {
+        let key = "\(red)\(green)\(blue)\(alpha)"
+        if let color = colorStore[key] {
+            return color
+        }
+        let color = UIColor(
+            red: red,
+            green: green,
+            blue: blue,
+            alpha: alpha
+        )
+        colorStore[key] = color
+        return color
+    }
+}
